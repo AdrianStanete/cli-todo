b5b6e341bdf774a756cbe3af1dbee6dd0c37a702
Adrian Stanete <adrianstanete@icloud.com>
2021-12-14 16:22:35 +0000

    create-print-exit-functions.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import click
 
 taskcounter = 0
+tasksarray = []
 
-#Create a new task
-def newtask(): 
-    input("Task: ")
-    taskcounter = taskcounter +1  #I want to add +1 to the task counter but can´t
-    print("You have ", taskcounter,"tasks")
+def print_main_interface():
+    print("Welcome to this shiny to-do app. What do you want to do?")
+    print("t to create a new task")
+    print("p to print your tasks")
+    print("x to exit")
+
+def create_new_task(): 
+    task = input("Task: ")
+    tasksarray.append(task)
+    taskcounter = +1  
+
+print_main_interface()
+while True:
+    
+    if input() == "t":
+        create_new_task()
+    
+    if input() == "p":
+        print(tasksarray)
 
 
-#Main interface
-print("Welcome to tasks")
-print("Press t to create a new task")
-
-
-answer = input()
-
-
-if answer == "t":
-    newtask()
+    if input() == "x":
+        break
